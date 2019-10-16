7b8ad52b9af30cdd524e38adc70069172a518d8b
CHAI53 <skymise@gmail.com>
2019-10-16 12:43:37 +0900

    댓글달기 엔터이벤트 추가

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ postingBtn.onclick = function () {
     element1.appendChild(element2);
     element1.appendChild(element3);
 };
+
+makingCom.onkeypress = function (event) {
+    if(event.keyCode === 13){
+        let makingComv = makingCom.value;
+        let element1 = document.createElement('div');
+        let element2 = document.createElement('span');
+        element2.innerHTML = 'rovvxhyo' + '&nbsp';
+        element2.style.fontWeight = 'bold';
+        let element3 = document.createElement('span');
+        element3.innerHTML = makingComv;
+        newMention.appendChild(element1)
+        element1.appendChild(element2);
+        element1.appendChild(element3);
+    }
+};
